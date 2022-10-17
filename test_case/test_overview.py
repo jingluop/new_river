c8a711b33177654b9b382a162d0efbf3b46dfb7e
fangpeng <1315644612@qq.com>
2022-10-17 16:56:28 +0800

    ethereum

--- test_case/test_overview.py
+++ test_case/test_overview.py
@@ -19,7 +19,7 @@ class TestOverview:
 
     @pytest.mark.parametrize('test_data', get_yaml_data('test_overview.yaml', 'count_collection_num'))
     def test_count_collection_num(self, test_data):
-        logger.info("测试数据为：{}".format(test_data))
+        logger.info("集合总数量测试数据为：{}".format(test_data))
         res = overview.count_collection_num()
         assert res['code'] == test_data['code']
         count_interface = res['data']
@@ -30,7 +30,7 @@ class TestOverview:
 
     @pytest.mark.parametrize('test_data', get_yaml_data('test_overview.yaml', 'market_cap_and_volume'))
     def test_market_cap_and_volume(self, test_data):
-        logger.info("测试数据为：{}".format(test_data))
+        logger.info("总市值和总交易量测试数据为：{}".format(test_data))
         res = overview.market_cap_and_volume_app(
             params={"timeRange": test_data['timeRange']} if 'timeRange' in test_data else '')
         if 'code' in test_data:
@@ -40,7 +40,7 @@ class TestOverview:
 
     @pytest.mark.parametrize('test_data', get_yaml_data('test_overview.yaml', 'heat_map'))
     def test_heat_map(self, test_data):
-        logger.info("测试数据为：{}".format(test_data))
+        logger.info("热力图测试数据为：{}".format(test_data))
         params = {}
         if 'timeRange' in test_data:
             params['timeRange'] = test_data['timeRange']
@@ -54,3 +54,24 @@ class TestOverview:
         if 'rise_list_len' in test_data:
             assert test_data['rise_list_len'] == len(res['data']['riseList'])
             assert test_data['fall_list_len'] == len(res['data']['fallList'])
+
+    @pytest.mark.parametrize('test_data', get_yaml_data('test_overview.yaml', 'top_ten'))
+    def test_top_ten(self, test_data):
+        logger.info("top ten测试数据为：{}".format(test_data))
+
+    @pytest.mark.parametrize('test_data', get_yaml_data('test_overview.yaml', 'ethereum'))
+    def test_ethereum(self, test_data):
+        logger.info("ethereum测试数据为：{}".format(test_data))
+        params = {}
+        if 'timeRange' in test_data:
+            params['timeRange'] = test_data['timeRange']
+        if 'type' in test_data:
+            params['type'] = test_data['type']
+        res = overview.ethereum_app(params=params)
+        if 'code' in test_data:
+            assert test_data['code'] == res['code']
+        else:
+            assert test_data['status'] == res['status']
+        if 'len_list' in test_data:
+            for name in test_data['len_list_name'].split(','):
+                assert test_data['len_list'] == len(res['data'][name])
