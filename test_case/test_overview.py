4313ec5489b287907747dfd03ea2ba54fa784e52
fangpeng <1315644612@qq.com>
2022-10-17 14:33:20 +0800

    heat map

--- test_case/test_overview.py
+++ test_case/test_overview.py
@@ -24,13 +24,15 @@ class TestOverview:
         assert res['code'] == test_data['code']
         count_interface = res['data']
         count_sql = test_data['sql_data'][0]['count']
-        logger.info("首页overview统计集合总数接口测试数据为（数据库查询，接口返回）：[{}，{}]".format(count_sql, count_interface))
+        logger.info(
+            "首页overview统计集合总数接口测试数据为（数据库查询，接口返回）：[{}，{}]".format(count_sql, count_interface))
         assert count_sql == count_interface
 
     @pytest.mark.parametrize('test_data', get_yaml_data('test_overview.yaml', 'market_cap_and_volume'))
     def test_market_cap_and_volume(self, test_data):
         logger.info("测试数据为：{}".format(test_data))
-        res = overview.market_cap_and_volume_app(params={"timeRange": test_data['timeRange']} if 'timeRange' in test_data else '')
+        res = overview.market_cap_and_volume_app(
+            params={"timeRange": test_data['timeRange']} if 'timeRange' in test_data else '')
         if 'code' in test_data:
             assert test_data['code'] == res['code']
         else:
@@ -39,8 +41,16 @@ class TestOverview:
     @pytest.mark.parametrize('test_data', get_yaml_data('test_overview.yaml', 'heat_map'))
     def test_heat_map(self, test_data):
         logger.info("测试数据为：{}".format(test_data))
-        res = overview.heat_map_app(params={"timeRange": test_data['timeRange']} if 'timeRange' in test_data else '')
+        params = {}
+        if 'timeRange' in test_data:
+            params['timeRange'] = test_data['timeRange']
+        if 'dataNum' in test_data:
+            params['dataNum'] = test_data['dataNum']
+        res = overview.heat_map_app(params=params)
         if 'code' in test_data:
             assert test_data['code'] == res['code']
         else:
             assert test_data['status'] == res['status']
+        if 'rise_list_len' in test_data:
+            assert test_data['rise_list_len'] == len(res['data']['riseList'])
+            assert test_data['fall_list_len'] == len(res['data']['fallList'])
