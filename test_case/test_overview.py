b2235e5b0daa2b395595862231c1c7e08ed2fa0d
fangpeng <1315644612@qq.com>
2022-10-17 13:42:42 +0800

    market cap and volume

--- test_case/test_overview.py
+++ test_case/test_overview.py
@@ -17,7 +17,7 @@ api_root_url = ReadFileData().load_ini(data_file_path)["host"]["api_root_url"]
 
 class TestOverview:
 
-    @pytest.mark.parametrize('test_data', get_yaml_data('test_overview.yaml', 'count_collection_num', db_type='mysql'))
+    @pytest.mark.parametrize('test_data', get_yaml_data('test_overview.yaml', 'count_collection_num'))
     def test_count_collection_num(self, test_data):
         logger.info("测试数据为：{}".format(test_data))
         res = overview.count_collection_num()
@@ -27,4 +27,20 @@ class TestOverview:
         logger.info("首页overview统计集合总数接口测试数据为（数据库查询，接口返回）：[{}，{}]".format(count_sql, count_interface))
         assert count_sql == count_interface
 
-    # def test_
+    @pytest.mark.parametrize('test_data', get_yaml_data('test_overview.yaml', 'market_cap_and_volume'))
+    def test_market_cap_and_volume(self, test_data):
+        logger.info("测试数据为：{}".format(test_data))
+        res = overview.market_cap_and_volume_app(params={"timeRange": test_data['timeRange']} if 'timeRange' in test_data else '')
+        if 'code' in test_data:
+            assert test_data['code'] == res['code']
+        else:
+            assert test_data['status'] == res['status']
+
+    @pytest.mark.parametrize('test_data', get_yaml_data('test_overview.yaml', 'heat_map'))
+    def test_heat_map(self, test_data):
+        logger.info("测试数据为：{}".format(test_data))
+        res = overview.heat_map_app(params={"timeRange": test_data['timeRange']} if 'timeRange' in test_data else '')
+        if 'code' in test_data:
+            assert test_data['code'] == res['code']
+        else:
+            assert test_data['status'] == res['status']
